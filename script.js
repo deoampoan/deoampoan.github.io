c0ca940e98c5b76b9367170d8633a064e3fcd82e
deoampoan <ampoandeo@gmail.com>
2024-02-15 00:22:14 -0800

    about

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-document.getElementById('contactBtn').addEventListener('click', function() {
-    var contactInfo = document.getElementById('contactInfo');
-    if (contactInfo.style.display === 'none') {
-        contactInfo.style.display = 'block';
-    } else {
-        contactInfo.style.display = 'none';
-    }
-});
+
 function sendEmail() {
     window.open('gwapzdhie@gmail.com');
+};
+
+const carousel = document.querySelector('.carousel');
+let currentIndex = 0;
+
+function updateCarouselWidth() {
+    const carouselWidth = carousel.offsetWidth;
+    carousel.computedStyleMap.transform = 'translateX(-${currentIndex * carouselWidth}px)';
 }
+
+window.addEventListener('resize', updateCarouselWidth);
+
+document.querySelector('.prev-btn').addEventListener('click', () => {
+    currentIndex = Math.max(currentIndex -1, 0);
+    updateCarouselWidth();
+});
+
+document.querySelector('.next-btn').addEventListener('click', () => {
+    currentIndex = Math.min(currentIndex +1, 4);
+    updateCarouselWidth();
+});

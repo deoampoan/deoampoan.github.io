18d641f74ea65a517fb39c09c63028fcb01b81bb
deoampoan <ampoandeo@gmail.com>
2024-02-16 00:22:02 -0800

    slideshow

--- script.js
+++ script.js
@@ -1,24 +1,88 @@
-
 function sendEmail() {
     window.open('gwapzdhie@gmail.com');
 };
 
-const carousel = document.querySelector('.carousel');
-let currentIndex = 0;
+const slideshow = document.querySelector('.slideshow'),
+   slides = Array.from(document.querySelectorAll('.slide'))
 
-function updateCarouselWidth() {
-    const carouselWidth = carousel.offsetWidth;
-    carousel.computedStyleMap.transform = 'translateX(-${currentIndex * carouselWidth}px)';
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID = 0,
+  currentIndex = 0
+
+slides.forEach((slide, index) => {
+    const slideImage = slide.querySelector('img')
+    slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+
+    // Touch events
+    slide.addEventListener('touchstart', touchStart('index'))
+    slide.addEventListener('touchend', touchEnd)
+    slide.addEventListener('touchmove', touchMove)
+
+    // Mouse events
+    slide.addEventListener('mousedown', touchStart('index'))
+    slide.addEventListener('mouseup', touchEnd)
+    slide.addEventListener('mouseleave', touchEnd)
+    slide.addEventListener('mousemove', touchMove)
+})
+
+// Disable context menu
+window.oncontextmenu = function(event){
+    event.preventDefault()
+    event.stopPropagation()
+    return false
 }
 
-window.addEventListener('resize', updateCarouselWidth);
+function  touchStart(index) {
+    return function(event) {
+        currentIndex = index
+        startPos = getPositionX(event)
+        isDragging = true
 
-document.querySelector('.prev-btn').addEventListener('click', () => {
-    currentIndex = Math.max(currentIndex -1, 0);
-    updateCarouselWidth();
-});
+        slideshow.classList.add('grabbing')
+    }
+}
 
-document.querySelector('.next-btn').addEventListener('click', () => {
-    currentIndex = Math.min(currentIndex +1, 4);
-    updateCarouselWidth();
-});
+function touchEnd() {
+    isDragging = false
+    cancelAnimationFrame(animationID)
+
+    const movedBy =  currentTranslate - prevTranslate
+
+    if(movedBy < -100 && currentIndex < slides.length - 1)
+    currentIndex += 1
+
+    if(movedBy > 100 && currentIndex > 0) currentIndex -= 1
+
+    setPositionByIndex()
+
+    slideshow.classList.remove('grabbing')
+}
+
+function touchMove(event) {
+    if(isDragging) {
+        const currentPosition = getPositionX(event)
+        currentTranslate = prevTranslate + currentPosition - startPos
+    }
+}
+
+function getPositionX(event) {
+    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+}
+
+function animation() {
+    setSliderPosition()
+    if(isDragging)  requestAnimationFrame(animation)
+}
+
+function setSliderPosition() {
+    slideshow.style.transform = 'translateX(${currentTranslate}px)'
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -window.innerWidth
+    prevTranslate = currentTranslate
+    setSliderPosition()
+}
